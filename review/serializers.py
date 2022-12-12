1f5d2514b700a05b79bb0ae4ab9b8cd7d7724774
baraev1993 <baraev1993@gmail.com>
2022-12-12 10:44:26 +0600

    serializers views

---
+++ review/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework.serializers import ModelSerializer
+from . models import Comment, Rating
+
+
+class CommentSerializer(ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+class RatingSerializer(ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = '__all__'
+
+
