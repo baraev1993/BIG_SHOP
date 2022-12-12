c4884f798a170810ead39deed3340020865263c1
baraev1993 <baraev1993@gmail.com>
2022-12-12 12:33:32 +0600

    product admin inlines

--- main/models.py
+++ main/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.title
 
