75a3e68396eb0669bdf61f70c2a9873342a90157
baraev1993 <baraev1993@gmail.com>
2023-01-12 15:06:47 +0600

    ifix

--- config/settings.py
+++ config/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -188,4 +188,5 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:3001',
     'https://www.thunderclient.com',
+    'bigshop-production.up.railway.app',    
 ]
